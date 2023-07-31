60b524a059bee0a3989d4611982bb2013cf177dd
Alper Alkan <alper.alkan2000@gmail.com>
2023-07-31 18:35:05 +0200

    use sharp 0.27.1 for comp mode

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 
 # Ensures old CPU compability. See https://github.com/lovell/sharp/issues/3743.
 if ! cat /proc/cpuinfo | grep -q "avx\|sse4_2"; then
-  echo "Force installing sharp@0.27.2 to ensure compability with older CPUs"
-  pnpm install sharp@0.27.2
+  echo "Force installing sharp@0.27.1 to ensure compability with older CPUs"
+  pnpm install sharp@0.27.1
 fi
 
 # If running as root, it means the --user directive for Docker CLI/Compose was not used
