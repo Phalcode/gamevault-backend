3918da215cac29d990f69d8e2f2ff8f6f25053d6
Alper Alkan <alper.alkan2000@gmail.com>
2023-08-02 14:45:20 +0200

    change user add and remove syntax

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 set -e
 
+
+# Ensures old CPU compability. See https://github.com/lovell/sharp/issues/3743.
+if ! cat /proc/cpuinfo | grep -q "avx\|sse4_2"; then
+    echo "Force installing sharp@0.25.4 to ensure compability with older CPUs"
+    pnpm install sharp@0.25.4
+fi
+
 # If running as root, it means the --user directive for Docker CLI/Compose was not used
-# Use the PUID env then
+# Use then the PUID env
 if [ "$(id -u)" = '0' ]; then
-    deluser node
-    addgroup -S node -g $PGID
-    adduser -S -G node -u $PUID node
+    groupmod -o -g "$PGID" node
+    usermod -o -u "$PUID" node
     sudo -u "#$PUID" -g "#$PGID" node "$@"
-else
+else # if using the user directive, run normally
     exec node "$@"
 fi
