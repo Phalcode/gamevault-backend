35a67d01d5a982ba7b9deaea479f9eaf2cc24e80
Alper Alkan <alper.alkan2000@gmail.com>
2023-09-26 15:04:30 +0200

    conflicting users check

--- src/modules/database/migrations/sqlite/1695686400000-check-user-case-conflicts.ts
+++ src/modules/database/migrations/sqlite/1695686400000-check-user-case-conflicts.ts
@@ -1,30 +1,25 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
-import { GamevaultUser } from "../../../users/gamevault-user.entity";
-
 export class CheckUserCaseConflicts1695686400000 implements MigrationInterface {
   name?: string;
   transaction?: boolean;
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     // Check for conflicting users with different casing for username or email
-    const conflictingUsers = await queryRunner.connection
-      .getRepository(GamevaultUser)
-      .createQueryBuilder("gamevault_user")
-      .select([
-        "gamevault_user.id",
-        "gamevault_user.username",
-        "gamevault_user.email",
-      ])
-      .addSelect("COUNT(*)", "count")
-      .groupBy("LOWER(gamevault_user.username)")
-      .addGroupBy("LOWER(gamevault_user.email)")
-      .having("COUNT(*) > 1")
-      .getRawMany();
+    const duplicateUsers = await queryRunner.query(`
+            SELECT u.username, u.email
+            FROM gamevault_user u
+            WHERE (LOWER(u.username), LOWER(u.email)) IN (
+                SELECT LOWER(username), LOWER(email)
+                FROM gamevault_user
+                GROUP BY LOWER(username), LOWER(email)
+                HAVING COUNT(*) > 1
+            );
+        `);
 
-    if (conflictingUsers.length > 0) {
+    if (duplicateUsers.length > 0) {
       throw new Error(
         `User conflicts detected with different casing for username or email. Please roll back to GameVault Backend version 6.0.0 and resolve the conflicts before updating to v7.0.0.\nConflicting Users: ${JSON.stringify(
-          conflictingUsers,
+          duplicateUsers,
           null,
           2,
         )}`,
