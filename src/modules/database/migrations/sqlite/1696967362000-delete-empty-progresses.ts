982af34990ccaa06075dc6497b39ee7a7bd13aa5
Alper Alkan <alper.alkan2000@gmail.com>
2023-10-18 16:49:44 +0200

    fix migration

--- src/modules/database/migrations/sqlite/1696967362000-delete-empty-progresses.ts
+++ src/modules/database/migrations/sqlite/1696967362000-delete-empty-progresses.ts
@@ -1,6 +1,4 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
-import { Progress } from "../../../progress/progress.entity";
-import { State } from "../../../progress/models/state.enum";
 import { Logger } from "@nestjs/common";
 export class DeleteEmptyProgresses1696967362000 implements MigrationInterface {
   private readonly logger = new Logger(DeleteEmptyProgresses1696967362000.name);
@@ -8,15 +6,8 @@ export class DeleteEmptyProgresses1696967362000 implements MigrationInterface {
   transaction?: boolean;
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    // Check for State Unplayed progresses with 0 Minutes.
-    const progressRepository = queryRunner.connection.getRepository(Progress);
-    const progressesToDelete = await progressRepository.findBy({
-      minutes_played: 0,
-      state: State.UNPLAYED,
-    });
-    progressRepository.remove(progressesToDelete);
-    this.logger.log(
-      `Database Migration deleted ${progressesToDelete.length} empty progresses from your database.`,
+    await queryRunner.query(
+      `DELETE FROM "progress" WHERE minutes_played = 0 AND state = 'UNPLAYED';`,
     );
   }
 
