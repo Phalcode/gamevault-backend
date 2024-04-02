9c7b0cd7cb6013f75f86e9e1fdf09945dbf923a2
Alper Alkan <alper.alkan2000@gmail.com>
2024-04-03 00:37:27 +0200

    create and permit default folders in dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,10 @@
 FROM node:lts-slim AS base
+
 # Default Variables
 ENV PUID=1000
 ENV PGID=1000
 ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true
+
 # Build time variables
 ## Allow non-root usage
 ENV NPM_CONFIG_PREFIX=/home/node/.npm-global
@@ -10,38 +12,55 @@ ENV PATH=$PATH:/home/node/.npm-global/bin
 ENV PNPM_HOME=/pnpm
 ENV PATH=$PNPM_HOME:$PATH
 ENV SERVER_PORT=8080
+
+# Create directories and set open permissions
 VOLUME /files /images /logs /db
+RUN mkdir -p /files /images /logs /db \
+    && chmod -R 777 /files /images /logs /db
+
 # Install pnpm and other needed tools
-RUN sed -i -e's/ main/ main non-free non-free-firmware contrib/g' /etc/apt/sources.list.d/debian.sources \ 
+RUN sed -i -e's/ main/ main non-free non-free-firmware contrib/g' /etc/apt/sources.list.d/debian.sources \
     && apt update \
     && apt install -y sudo tzdata curl p7zip-full p7zip-rar postgresql-client \
+    && apt clean \
     && npm i -g pnpm
+
 WORKDIR /app
 
 FROM base AS build
+
 # Copy files only needed for install
 COPY package.json pnpm-lock.yaml ./
 RUN pnpm install --frozen-lockfile
+
 # Copy everything for building
 COPY . .
 RUN pnpm run build
 
 FROM base AS prod-deps
+
 COPY package.json pnpm-lock.yaml ./
 RUN pnpm install --prod --frozen-lockfile
 
 FROM base AS release
+
 ENV NODE_ENV=production
+
 COPY package.json pnpm-lock.yaml ./
+
 # Chown /app to the original node user (1000)
 # As only read is needed this is fine when using --user or PUID
 COPY --from=build --chown=node:node /app/dist ./dist
 COPY --from=prod-deps --chown=node:node /app/node_modules ./node_modules
+
 # Entry script for providing dynamic env changes like PUID
 COPY entrypoint.sh /usr/local/bin/
 RUN chmod +x /usr/local/bin/entrypoint.sh
+
 EXPOSE ${SERVER_PORT}/tcp
+
 # Periodic Healthcheck on /api/v1/health
 HEALTHCHECK CMD curl -f http://localhost:${SERVER_PORT}/api/health || exit
+
 ENTRYPOINT ["/usr/local/bin/entrypoint.sh"]
 CMD [ "dist/src/main" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,45 +1,16 @@
 #!/bin/sh
 set -e
 
-# Function to create a directory if it does not exist and set the proper ownership
-createDirectoryIfNotExist() {
-  if [ ! -d "$1" ]; then
-    echo "Directory \"$1\" does not exist. Trying to create a new one..."
-    sudo mkdir -p "$1"
-    sudo chown "$PUID":"$PGID" "$1"
-  fi
-}
-
-# Check and create necessary folders if they do not exist
-check_folders() {
-  if [ "$MOCK_FILES" = "true" ]; then
-    echo "Not checking or creating any folders because MOCK_FILES is set to true"
-    return
-  fi
-
-  # Set default values if environment variables are not set or are empty
-  [ -z "$FILES_VOLUME" ] && FILES_VOLUME="/files"
-  [ -z "$IMAGES_VOLUME" ] && IMAGES_VOLUME="/images"
-  [ -z "$LOGS_VOLUME" ] && LOGS_VOLUME="/logs"
-  [ -z "$SQLITEDB_VOLUME" ] && SQLITEDB_VOLUME="/db"
-
-  createDirectoryIfNotExist "$FILES_VOLUME"
-  createDirectoryIfNotExist "$IMAGES_VOLUME"
-  createDirectoryIfNotExist "$LOGS_VOLUME"
-  createDirectoryIfNotExist "$SQLITEDB_VOLUME"
-}
-
-# Perform directory checks
-check_folders
-
-# Existing logic for user permissions and running the node process
+# If running as root, it means the --user directive for Docker CLI/Compose was not used
+# Use then the PUID env to set the user and group IDs
 if [ "$(id -u)" = '0' ]; then
+    # Modify the group ID of the 'node' user to match the PGID environment variable
     groupmod -o -g "$PGID" node
+    # Modify the user ID of the 'node' user to match the PUID environment variable
     usermod -o -u "$PUID" node
-    # Ensure that all specified directories are owned by the node user
-    sudo chown -R "$PUID":"$PGID" "$FILES_VOLUME" "$IMAGES_VOLUME" "$LOGS_VOLUME" "$SQLITEDB_VOLUME"
-    # Execute the command as the node user
+    # Run the specified command with the modified user and group IDs
     sudo -u "#$PUID" -g "#$PGID" -E node "${@}"
-else # if using the user directive, run normally
+else
+    # If using the user directive, run the specified command normally
     exec node "${@}"
 fi
