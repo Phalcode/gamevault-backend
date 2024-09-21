418a7dab1d93a199a6e0aa8214e4d32b326de40e
Alper Alkan <alper.alkan2000@gmail.com>
2024-09-22 01:39:11 +0200

    support older routes

--- src/middleware/legacy-routes.middleware.ts
+++ src/middleware/legacy-routes.middleware.ts
@@ -2,10 +2,13 @@ import { Injectable, NestMiddleware } from "@nestjs/common";
 import { NextFunction, Request, Response } from "express";
 
 @Injectable()
-export class ApiVersionMiddleware implements NestMiddleware {
+export class LegacyRoutesMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     // Replace /api/v1/ with /api/ in the request URL
     req.url = req.url.replace("/api/v1/", "/api/");
+
+    // Redirect /api/files/reindex to newer /api/games/reindex"
+    req.url = req.url.replace("/api/files/reindex", "/api/games/reindex");
 
     next();
   }
