aeacae6a6bf0a827c92852659b2646393aeab86a
Alper Alkan <alper.alkan2000@gmail.com>
2025-09-15 23:52:15 +0200

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -42,6 +42,7 @@ async function bootstrap(): Promise<void> {
   if (configuration.SERVER.CORS_ALLOWED_ORIGINS.length) {
     app.enableCors({
       origin: configuration.SERVER.CORS_ALLOWED_ORIGINS,
+      allowedHeaders: "*",
       credentials: true,
       methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     });
